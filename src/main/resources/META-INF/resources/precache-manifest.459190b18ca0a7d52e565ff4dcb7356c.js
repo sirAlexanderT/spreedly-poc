821a75ab7ed899020314dc5d00f756564352d822
Alexander TIndi <>
2020-08-06 22:11:59 +0100

    updated application to use port 8085

---
+++ src/main/resources/META-INF/resources/precache-manifest.459190b18ca0a7d52e565ff4dcb7356c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb6663c23d8427d23d4c5c77326653e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8503a6dd1a8f936cb04f",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "6ecf119cbe6a9a285b75",
+    "url": "/static/css/main.0b0db598.chunk.css"
+  },
+  {
+    "revision": "8503a6dd1a8f936cb04f",
+    "url": "/static/js/2.8a0445ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a0445ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ecf119cbe6a9a285b75",
+    "url": "/static/js/main.0a7b3478.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  }
+]);
---
+++ src/main/resources/META-INF/resources/precache-manifest.5508a1669e56e78babb46a756a307f33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749469ac9a94c3e9c931e444bb50005a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8503a6dd1a8f936cb04f",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "0d447ac801fc9724bdf3",
+    "url": "/static/css/main.0b0db598.chunk.css"
+  },
+  {
+    "revision": "8503a6dd1a8f936cb04f",
+    "url": "/static/js/2.8a0445ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a0445ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d447ac801fc9724bdf3",
+    "url": "/static/js/main.a0211851.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  }
+]);
